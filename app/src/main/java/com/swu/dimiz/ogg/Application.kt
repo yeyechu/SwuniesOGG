6c6247fd80ce9bc538c1b2b9fa65cf8d12bec151
yeyechu <87848758+yeyechu@users.noreply.github.com>
2023-08-19 20:03:13 +0900

    파이어베이스 추가 중1

---
+++ app/src/main/java/com/swu/dimiz/ogg/Application.kt
@@ -0,0 +1,36 @@
+package com.swu.dimiz.ogg
+
+import androidx.multidex.MultiDexApplication
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Application: MultiDexApplication() {
+
+    companion object {
+        // 파이어베이스 인증 상태 확인
+        lateinit var auth: FirebaseAuth
+        var email: String? = null
+
+        fun checkAuth(): Boolean {
+            val currentUser = auth.currentUser
+            return currentUser?.let {
+
+                email = currentUser.email
+
+                if(currentUser.isEmailVerified) {
+                    true
+                }
+                false
+
+            } ?: let {
+                false
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        auth = Firebase.auth
+    }
+}
