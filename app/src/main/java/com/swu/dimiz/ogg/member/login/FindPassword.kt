90db5445946d0a060bfe75e3d06116201ca5f39a
Voming <68582465+Voming@users.noreply.github.com>
2023-10-19 19:52:45 +0900

    비밀번호 재설정 완료

--- app/src/main/java/com/swu/dimiz/ogg/member/login/FindPassword.kt
+++ app/src/main/java/com/swu/dimiz/ogg/member/login/FindPassword.kt
@@ -1,15 +1,19 @@
 package com.swu.dimiz.ogg.member.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.swu.dimiz.ogg.R
 import com.swu.dimiz.ogg.databinding.FragmentSigninFindPasswordBinding
 import timber.log.Timber
@@ -28,6 +32,19 @@ class FindPassword: Fragment() {
     ): View? {
         _binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_signin_find_password, container, false)
+
+        binding.sendEmailBtn.setOnClickListener(){
+            val email =  binding.emailEt.editText?.text.toString()
+
+            Firebase.auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Timber.i("Email sent.")
+                        Toast.makeText(activity,"이메일로 비밀번호 재설정 링크 보냈어요", Toast.LENGTH_SHORT).show()
+                        //todo 화면 이동
+                    }
+                }
+        }
 
         return binding.root
     }
